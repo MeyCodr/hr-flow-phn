bff81884119d7b26f82adee3c269e564b7922f06
Amir Anwar <amiyer1803@gmail.com>
2026-02-06 11:57:07 +0800

    06022026-prisma-connection-fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,23 @@
-
-import "dotenv/config";
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+// lib/prisma.ts
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@/generated/client";
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// create adapter
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 5
+  connectionLimit: 10, // increase for VPS
 });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+// use shared client
+export const prisma =
+  global.prisma ||
+  new PrismaClient({ adapter, log: ["query", "warn", "error"] });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
